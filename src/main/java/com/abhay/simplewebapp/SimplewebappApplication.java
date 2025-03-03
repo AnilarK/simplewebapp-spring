277cdf6039d55266a04d43131c54a9b3d1529b31
AbhayZolostays <abhay_s@zolostays.com>
2025-03-03 12:00:03 +0530

    changes

--- src/main/java/com/abhay/simplewebapp/SimplewebappApplication.java
+++ src/main/java/com/abhay/simplewebapp/SimplewebappApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimplewebappApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SimplewebappApplication.class, args);
 	}
